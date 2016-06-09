fa730e97ec3544194b0e6bb36060feb909ab25b7
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-06-09 00:12:25 -0500

    more js changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,12 +16,28 @@ $("#addTrainBtn").on("click", function(){
 	// Grabs user input
 	var trainName = $("#trainNameInput").val().trim();
 	var trainDestination = $("#destinationInput").val().trim();
-	var firstTrain = moment($("#firstTrainTimeInput").val().trim(), "HH:mm").format("HHmm");
+	var firstTrain = moment($("#firstTrainTimeInput").val().trim(), "HH:mm").format("HH:mm");
 	var trainFrequency = $("#frequencyInput").val().trim();
 
-  // Get current time to calc minutes away
-  var timeNow = moment().format("HHmm");
+  // // Get current time to calc minutes away
+  var currentTime = moment().format("HH:mm");
+  console.log("Current Time: " + currentTime);
 
+  // calc difference in times
+  var diffTime = moment().diff(moment(firstTrain), "minutes");
+  console.log("Difference in time: " + diffTime);
+
+  // Time apart
+  var tRemainder = diffTime % trainFrequency;
+  console.log(tRemainder);
+
+  // Minutes Until Train
+  var tMinutesTillTrain = trainFrequency - tRemainder;
+  console.log("Minutes Till Train: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("Arrival Time: " + moment(nextTrain).format("HH:mm"));
 
   // Object to hold train data
   var newTrain = {
@@ -34,12 +50,6 @@ $("#addTrainBtn").on("click", function(){
 
   // Upload train data to database
   trainData.push(newTrain);
-
-  console.log(newTrain.name);
-	console.log(newTrain.destination);
-	console.log(newTrain.start);
-	console.log(newTrain.frequency);
-  console.log(newTrain.minsAway);
 
   swal({title: "Chooo Choo!",   text: "You have successfully added a train!",   type: "success", confirmButtonColor: "#2ecc71",   confirmButtonText: "Okay", closeOnConfirm: true });
 
